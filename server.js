39d7c5759f60882c53ed323e2175d984b805d116
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-06-03 18:28:31 +0530

    error handling mongo db middleware

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
-
+app.use(express.json()); //it will provide a parser which will help us to parse the data stream which we receive from client
 app.use("/api/contacts", require("./routes/contactRoutes")); //middleware
-
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
