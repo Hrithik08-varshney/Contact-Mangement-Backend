91cd86d66dbc0df08f9c868b4fed33f143c101c4
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-06-06 23:14:44 +0530

    user schema and controllers added

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json()); //it will provide a parser which will help us to parse the data stream which we receive from client
 app.use("/api/contacts", require("./routes/contactRoutes")); //middleware
+app.use("/api/users", require("./routes/userRoutes")); //middleware
 app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
